af45a89cfdbd0010904f434e4e60a76847dce592
Ana Beatriz <anabiawalker@gmail.com>
2021-12-10 13:45:45 -0300

    Comentários em ClassEvents

--- src/util/ClassEvents.js
+++ src/util/ClassEvents.js
@@ -1,24 +1,34 @@
 export class ClassEvent{
     constructor(){
         this._events = {}
-
-
     }
-    on(eventName, fn){
+    //CRIANDO EVENTOS
+
+    on(eventName, fn){ //nome, função
+
+        //Se não existe o evento (se não está em uso), cria ele recebendo um array
         if(!this._events[eventName]) this._events[eventName] = new Array()
 
+        //Adiciona uma função ao evento
         this._events[eventName].push(fn)
+        //evento agora é um array
+
+        //Isso permite utilizar o mesmo evento várias vezes, com diferentes execuções (funções), fazendo com que todos funcionam e não sejam subscritos
     }
 
+    //Gatilho do evento
     trigger(){
-        let args = [...arguments]
-        let eventName = args.shift()
+
+        let args = [...arguments] //pega os argumentos passados no parâmetros, sem fazer com que os parâmetros seja obrigatórios
+
+        let eventName = args.shift()//obriga o nome do evento a ser o primeiro argumento
 
         args.push(new Event(eventName))
 
-        if(this._events[eventName] instanceof Array){
+        if(this._events[eventName] instanceof Array){//se é um array
+
             this._events[eventName].forEach(fn =>{
-                fn.apply(null, args)
+                fn.apply(null, args)//faz com que execute a função 
             })
         }
     }
